eadd8a841e80343c66ec0b0c4c529f4363d27d2f
ivanmartinezsuarez <56915947+ivanmartinezsuarez@users.noreply.github.com>
2019-12-17 19:38:00 +0100

    Delete triangule_rectangle.m

--- Matlab_code/triangule_rectangle.m
+++
@@ -1,7 +0,0 @@
-function [S,T,BR,RT]=triangule_rectangle(rect,m,n,ref_mat,ref_bord)
-if(nargin==3) ref_mat=0;ref_bord=[1 2 3 4];end,
-if(nargin==4) ref_bord=[1 2 3 4];end,
-[S,T,BR,RT]=triangule_carre(m,n,ref_mat,ref_bord);
-%transf des coordonnées S
-l=rect(2)-rect(1);S(:,1)=rect(1)+l*S(:,1);
-l=rect(4)-rect(3);S(:,2)=rect(3)+l*S(:,2);
